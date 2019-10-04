08fbb9b8016c6737919ffb74f1897aac0d15ac88
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2019-10-04 18:50:44 -0500

    Updates

---
+++ precache-manifest.852cec878807141ab28ef23e73688e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f805b6a82572a1545026f6187783c8",
+    "url": "/OscarRobertRodriguez/hacker-news-react/index.html"
+  },
+  {
+    "revision": "0cf91335210b47a75a66",
+    "url": "/OscarRobertRodriguez/hacker-news-react/static/css/main.9abe6570.chunk.css"
+  },
+  {
+    "revision": "fec9d69cc9c55bee4344",
+    "url": "/OscarRobertRodriguez/hacker-news-react/static/js/2.235c962c.chunk.js"
+  },
+  {
+    "revision": "0cf91335210b47a75a66",
+    "url": "/OscarRobertRodriguez/hacker-news-react/static/js/main.836a8a37.chunk.js"
+  },
+  {
+    "revision": "5388f019c1e1c216b803",
+    "url": "/OscarRobertRodriguez/hacker-news-react/static/js/runtime-main.23a696c6.js"
+  }
+]);
